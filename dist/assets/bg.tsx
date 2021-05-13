ee5b5fab2855794cb2ab798822ad06488abda607
vale18 <emil.kiesche@gmail.com>
2021-05-13 12:31:38 +0200

    update, gitignore rausgenommen

---
+++ dist/assets/bg.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="bg" tilewidth="70" tileheight="70" tilecount="9" columns="3">
+ <image source="../../public/assets/tiles/platformer-art-complete-pack-0/Base pack/bg.png" width="256" height="256"/>
+</tileset>
