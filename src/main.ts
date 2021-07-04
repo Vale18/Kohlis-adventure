07139b5e42a512fdcefa5ab4aa60cca3b3519efc
vale18 <70882654+Vale18@users.noreply.github.com>
2021-07-04 23:06:24 +0200

    TitleScene(inProgress)

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import Phaser from 'phaser'
 
 import Game from './scenes/Game'
 import UI from './scenes/UI'
-
+import TitleScene from './scenes/TilteScene'
 const config: Phaser.Types.Core.GameConfig = {
 	type: Phaser.AUTO,
 	width: 600,
@@ -13,7 +13,7 @@ const config: Phaser.Types.Core.GameConfig = {
 			debug: true
 		}
 	},
-	scene: [Game, UI]
+	scene: [TitleScene, Game, UI]
 }
 
 export default new Phaser.Game(config)
